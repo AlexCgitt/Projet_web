e71ee113b66856385a0e8cc4fcce6bb46d87fa5f
AlexCgitt <alexmicrosoft03@gmail.com>
2024-06-24 20:32:48 +0200

    Acceuil numéro 1

---
+++ vues/Acceuil.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <link rel="stylesheet" href="../style/style_acceuil.css">
+    <title>Saint Quentree</title>
+</head>
+<body>
+    <nav>
+        <div class="logo">
+            <img src="image/logo.png" alt="logo">
+        </div>
+        <ul>
+            <li><a href="#">Ajout d'arbres</a></li>
+            <li><a href="#">Visualisation</a></li>
+            <li><a href="#">Prediction</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+        <div class="login">
+            <a href="#">log in / log out</a>
+        </div>
+    </nav>
+    
+    <main>
+        <h1>Saint Quentree</h1>
+        <p>Description du site</p>
+        <section class="grid-container">
+            <div class="grid-item">
+                <img src="image1.png" alt="Ajout d'arbres">
+                <h2>Ajout d'arbres</h2>
+                <p>Vous venez de planter un arbre, ajoutez-le au site</p>
+            </div>
+            <div class="grid-item">
+                <img src="image2.png" alt="Visualisation">
+                <h2>Visualisation</h2>
+                <p>Visualisez les arbres de la ville de Saint-Quentin dans un tableau ou sur une carte.</p>
+            </div>
+            <div class="grid-item">
+                <img src="image3.png" alt="Prediction">
+                <h2>Prediction</h2>
+                <p>Prédisez l'emplacement des arbres et leur cluster.</p>
+            </div>
+            <div class="grid-item">
+                <img src="image4.png" alt="Contact">
+                <h2>Contact</h2>
+                <p>En cas de problème, n'hésitez pas à contacter l'équipe.</p>
+            </div>
+        </section>
+    </main>
+
+    <footer>
+        <div class="contact-info">
+            <p>Contact</p>
+            <p>&copy; Saint Quentree</p>
+        </div>
+        <div class="map">
+            <img src="map.png" alt="Map">
+        </div>
+    </footer>
+</body>
+</html>
